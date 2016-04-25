38452e6db684ec3591c393f70e5db01d3bd3443e
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2016-04-25 10:48:27 +0800

    Load all devices in driver initialization
    initialize method will be called by neutron after all drivers
    have been loaded, it's the right place to do initialization work
    like loading devices.
    
    Change-Id: I5685faab3598b98523d7eb78847015e16ae3657e

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -18,6 +18,7 @@ from neutron.extensions import portbindings
 from neutron.plugins.ml2 import driver_api
 from oslo_log import log as logging
 
+from networking_generic_switch._i18n import _LI
 from networking_generic_switch import config as gsw_conf
 from networking_generic_switch import devices
 from networking_generic_switch import exceptions as exc
@@ -34,7 +35,13 @@ class GenericSwitchDriver(driver_api.MechanismDriver):
         been initialized. No abstract methods defined below will be
         called prior to this method being called.
         """
-        pass
+
+        gsw_devices = gsw_conf.get_devices()
+        self.switches = {}
+        for switch_info, device_cfg in gsw_devices.items():
+            switch = devices.device_manager(device_cfg)
+            self.switches[switch_info] = switch
+        LOG.info(_LI('Devices %s have been loaded'), self.switches.keys())
 
     def create_network_precommit(self, context):
         """Allocate resources for a new network.
@@ -68,9 +75,7 @@ class GenericSwitchDriver(driver_api.MechanismDriver):
 
         if provider_type == 'vlan' and segmentation_id:
             # Create vlan on all switches from this driver
-            gsw_devices = gsw_conf.get_devices()
-            for device_cfg in gsw_devices.values():
-                switch = devices.device_manager(device_cfg)
+            for switch in self.switches.values():
                 switch.add_network(segmentation_id, network_id)
 
     def update_network_precommit(self, context):
@@ -141,9 +146,7 @@ class GenericSwitchDriver(driver_api.MechanismDriver):
 
         if provider_type == 'vlan' and segmentation_id:
             # Delete vlan on all switches from this driver
-            gsw_devices = gsw_conf.get_devices()
-            for device_cfg in gsw_devices.values():
-                switch = devices.device_manager(device_cfg)
+            for switch in self.switches.values():
                 switch.del_network(segmentation_id)
 
     def create_subnet_precommit(self, context):
@@ -371,9 +374,8 @@ class GenericSwitchDriver(driver_api.MechanismDriver):
                                                      False)
         vnic_type = port['binding:vnic_type']
         if vnic_type == 'baremetal' and local_link_information:
-            gsw_devices = gsw_conf.get_devices()
             switch_info = local_link_information[0].get('switch_info')
-            if switch_info not in gsw_devices:
+            if switch_info not in self.switches:
                 raise exc.GenericSwitchConfigError(switch=switch_info)
             port_id = local_link_information[0].get('port_id')
             segments = context.segments_to_bind
@@ -386,9 +388,9 @@ class GenericSwitchDriver(driver_api.MechanismDriver):
                           port=port_id,
                           switch_info=switch_info,
                           segmentation_id=segmentation_id))
-            switch = devices.device_manager(gsw_devices[switch_info])
             # Move port to network
-            switch.plug_port_to_network(port_id, segmentation_id)
+            self.switches[switch_info].plug_port_to_network(port_id,
+                                                            segmentation_id)
             context.set_binding(segments[0][driver_api.ID],
                                 portbindings.VIF_TYPE_OTHER, {},
                                 status=const.PORT_STATUS_ACTIVE)
