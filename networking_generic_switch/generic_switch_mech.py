55679864b47fd64ef671d820bb663bf899b603c5
mcevik0 <mcevik@renci.org>
2018-03-06 16:11:18 -0500

    bump

--- networking_generic_switch/generic_switch_mech.py
+++ networking_generic_switch/generic_switch_mech.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
 from neutron.callbacks import resources
 from neutron.db import provisioning_blocks
 from neutron.plugins.ml2 import driver_api
